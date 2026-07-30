0c63f496f0bf8e84f6c85ecce85a4c9d59a00c03
Bhavna Sharma <bhavna@i2k2.com>
2026-07-30 14:03:01 +0530

    integrate web search api

--- src/tools/WebSearchTool.js
+++ src/tools/WebSearchTool.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export default class WebSearchTool {
 
     getDefinition() {
@@ -9,13 +11,39 @@ export default class WebSearchTool {
 
     async execute(query) {
 
-        // Call search API here
+        try {
 
-        return {
-            success: true,
-            results: []
-        };
+            const response = await axios.post(
+                "https://api.tavily.com/search",
+                {
+                    api_key: process.env.TAVILY_API_KEY,
+                    query: query,
+                    max_results: 5
+                }
+            );
 
+            console.log("Tavily Response:");
+            console.log(JSON.stringify(response.data, null, 2));
+
+            return {
+                success: true,
+                results: response.data.results || []
+            };
+
+        } catch (error) {
+
+            console.error("Tavily Error:");
+
+            if (error.response) {
+                console.error(error.response.data);
+            } else {
+                console.error(error.message);
+            }
+
+            return {
+                success: false,
+                error: error.message
+            };
+        }
     }
-
 }
